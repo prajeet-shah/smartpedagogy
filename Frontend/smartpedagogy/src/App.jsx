14ad59c187a9ea599a9354067bc564fb29ce95aa
prajeet shah <prajeetshah93@gmail.com>
2025-06-12 18:47:36 +0545

    configure project

---
+++ Frontend/smartpedagogy/src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl bg-yellow-500 ">hello world</h1>
+      <div className="text-5xl bg-amber-700">how are you????</div>
+    </div>
+  );
+}
+
+export default App;
