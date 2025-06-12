3ea0ac2d86cdf7b4ab0fcf499389269d82361407
prajeet shah <prajeetshah93@gmail.com>
2025-06-12 19:14:11 +0545

    managed folder and created server

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const express = require("express");
+
+const server = express();
+
+server.use("/", (req, res) => {
+  res.send("server is running...");
+});
+
+server.listen(7777, () => {
+  console.log("server is listening at port 7777");
+});
